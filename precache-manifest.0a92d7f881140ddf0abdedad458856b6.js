5f2c928679bd959fa2c8610fcf0c4dbe906b40b0
chung2014 <chung2014@gmail.com>
2020-06-03 17:01:29 +0800

    Updates

---
+++ precache-manifest.0a92d7f881140ddf0abdedad458856b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7160aa8baf7edea01056e8c927045cdc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ca8fdec53d01d7a91ccc",
+    "url": "/monsters-rolodex/static/css/main.33f7c044.chunk.css"
+  },
+  {
+    "revision": "2e92dffff18a4623c83b",
+    "url": "/monsters-rolodex/static/js/2.0da73388.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0da73388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8fdec53d01d7a91ccc",
+    "url": "/monsters-rolodex/static/js/main.b75b0dfe.chunk.js"
+  },
+  {
+    "revision": "0a7538a84e8ef0422b17",
+    "url": "/monsters-rolodex/static/js/runtime-main.984b980e.js"
+  }
+]);
